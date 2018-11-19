5d7a9ffdacb8fe3deb1fada8f66b2cad3591d1ca
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-19 13:41:45 +0100

    Setting DOM variables when the DOM is loaded

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,8 @@
-// This is where it all goes :)
+// declaring DOM variables
+let rock, paper, scissors
+// setting DOM variables when the DOM is loaded
+document.addEventListener('DOMContentLoaded', event => {
+    rock = document.querySelector('#rock')
+    paper = document.querySelector('#paper')
+    scissors = document.querySelector('#scissors')
+})
